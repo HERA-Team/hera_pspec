b0e0b5cda7f303ff8bf9b90e061318b5a4c6d3b0
Adrian Liu <acliu@berkeley.edu>
2018-01-08 13:40:17 -0800

    Created skeleton file for testing dataset.py

---
+++ hera_pspec/tests/test_dataset.py
@@ -0,0 +1,15 @@
+import unittest
+import nose.tools as nt
+import numpy as np
+import os
+import sys
+from hera_pspec.data import DATA_PATH
+from hera_pspec import conversions
+
+class Test_dataset(unittest.dataset):
+
+    def tearDown(self):
+        pass
+
+    def runTest(self):
+        pass
