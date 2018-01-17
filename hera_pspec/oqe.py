c8de05fbb3628cb9e24c465e56d78672a9e80ce8
Adrian Liu <acliu@berkeley.edu>
2018-01-16 17:17:26 -0800

    Wrote a whole bunch of tests for oqe.py

--- hera_pspec/oqe.py
+++ hera_pspec/oqe.py
@@ -93,17 +93,23 @@ def cov(d1, w1, d2=None, w2=None):
     cov : array_like
         covariance (or cross-variance) matrix of size (M,M)
     """
-    if d2 is None: d2,w2 = d1.conj(),w1
+    if d2 is None: d2,w2 = d1,w1
+    if not np.isreal(w1).all(): raise TypeError("Weight matrices must be real")
+    if not np.isreal(w2).all(): raise TypeError("Weight matrices must be real")
+    if np.less(w1, 0.).any() or np.less(w2, 0.).any(): raise ValueError("Weight matrices must be positive")
     d1sum,d1wgt = (w1*d1).sum(axis=1), w1.sum(axis=1)
     d2sum,d2wgt = (w2*d2).sum(axis=1), w2.sum(axis=1)
     x1,x2 = d1sum / np.where(d1wgt > 0,d1wgt,1), d2sum / np.where(d2wgt > 0,d2wgt,1)
     x1.shape = (-1,1)
     x2.shape = (-1,1)
-    d1x = d1 - x1
-    d2x = d2 - x2
-    C = np.dot(w1*d1x,(w2*d2x).conj().T)
+    # d1x = d1 - x1
+    # d2x = d2 - x2
+    C = np.dot(w1*d1,(w2*d2).conj().T)
+    #print "hey hey", C
     W = np.dot(w1,w2.T)
-    return C / np.where(W > 0, W, 1)
+    C /= np.where(W > 0, W, 1)
+    C -= np.outer(x1,x2.conj())
+    return C
 
 def lst_align(lsts, lstres=.001, interpolation='none'):
     """
