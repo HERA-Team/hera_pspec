97472cee5d1ea4eb72da34f78f7c3b22dabc1aa7
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-01-22 18:46:48 -0500

    fixed bugs in test_get_F but test not working for get_F.

--- hera_pspec/tests/test_pspecdata.py
+++ hera_pspec/tests/test_pspecdata.py
@@ -52,34 +52,48 @@ class Test_DataSet(unittest.TestCase):
         # 3) Sending in pure tones/sinusoids for x and y should give delta functions
 
     def test_get_F(self):
-        dfile=DATA_PATH+'zen.2458042.12552.xx.HH.uvXAA'
+        dpath=os.path.join(DATA_PATH,'zen.2458042.12552.xx.HH.uvXAA')
+        print dpath
         data=uv.UVData()
-        data.read_miriad(dfile)
+        wghts=uv.UVData()
+        data.read_miriad(dpath)
+        wghts.read_miriad(dpath)
         #set data to random white noise with a random variance and mean
         test_mean=np.abs(np.random.randn())
-        test_var=np.abs(np.random.randn())**2.
-        test_noise=np.random.randn(test_mean,test_var/2.,size=data.data_array.shape)\
-                  +1j*np.random.randn(test_mean,test_var/2.,size=data.data_array.shape)
+        test_var=np.abs(np.random.randn())
+        test_noise=np.random.randn(*data.data_array.shape)*test_var/np.sqrt(2.)+test_mean\
+                  +1j*np.random.randn(*data.data_array.shape)*test_var/np.sqrt(2.)+1j*test_mean
         data.data_array=test_noise#set data equal to test noise.
-        data.flag_array[:]=True#Make sure that all of the flags are set too true for analytic case. 
-        pspec=PSpecData()
-        pspec.add(data)#create pspec data set from data.
-        for j in range(1,len(pspec.dsets[0].Nbls)):
+        data.flag_array[:]=False#Make sure that all of the flags are set too true for analytic case.
+        wghts.data_array[:]=1.
+        wghts.flag_array[:]=False
+        pspec=pspecdata.PSpecData()
+        pspec.add([data],[wghts])#create pspec data set from data.
+        bllist=data.get_antpairs()
+        for j in range(1,pspec.dsets[0].Nbls):
+            #get baseline index
+            pair1=bllist[j]
             for k in range(j):
+                pair2=bllist[k]
                 #test equality
-                self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k)),
+                k1=(0,pair1[0],pair1[1],-5)
+                k2=(0,pair2[0],pair2[1],-5)
+                #print pspec.get_F(k1,k2)
+                #print pspec.get_F(k1,k2,use_identity=True)
+                #print pspec.get_F(k1,k2,true_fisher=True)
+                self.assertTrue(np.allclose(pspec.get_F(k1,k2),
                                             np.identity(pspec.dsets[0].Nfreqs)\
-                                            *pspec.dsets[0]**2./(2.*test_var),
+                                            *pspec.dsets[0].Nfreqs**2./(2.*test_var),
                                             rtol=2./pspec.dsets[0].Ntimes,
                                             atol=test_var*2./pspec.dsets[0].Ntimes))
                 #test for identity
-                self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k),use_identity=True),
+                self.assertTrue(np.allclose(pspec.get_F(k1,k2,use_identity=True),
                                             np.identity(pspec.dsets[0].Nfreqs)\
                                             *pspec.dsets[0].Nfreqs**2./2.))
                 self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k),use_identity=True),
                                             np.identity(pspec.dsets[0].Nfreqs)))
                 #test for true fisher
-                self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k),true_fisher=True),
+                self.assertTrue(np.allclose(pspec.get_F(k1,k2,true_fisher=True),
                                             np.identity(pspec.dsets[0].Nfreqs)\
                                             *pspec.dsets[0].Nfreqs**2./(2.*test_var),
                                             rtol=2./pspec.dsets[0].Ntimes,
