6984dfa764e1b59423f2e502c180ed2c58d955f0
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-01-22 16:33:35 -0500

    finished writing get_F tests.

--- hera_pspec/tests/test_pspecdata.py
+++ hera_pspec/tests/test_pspecdata.py
@@ -3,8 +3,10 @@ import nose.tools as nt
 import numpy as np
 import os
 import sys
-from hera_pspec.data import DATA_PATH
 from hera_pspec import pspecdata
+from hera_pspec import oqe
+from hera_pspec.data import DATA_PATH
+import pyuvdata as uv
 
 
 class Test_DataSet(unittest.TestCase):
@@ -49,6 +51,45 @@ class Test_DataSet(unittest.TestCase):
         self.assertAlmostEqual(xQx,1.)
         # 3) Sending in pure tones/sinusoids for x and y should give delta functions
 
+    def test_get_F(self):
+        dfile=DATA_PATH+'zen.2458042.12552.xx.HH.uvXAA'
+        data=uv.UVData()
+        data.read_miriad(dfile)
+        #set data to random white noise with a random variance and mean
+        test_mean=np.abs(np.random.randn())
+        test_var=np.abs(np.random.randn())**2.
+        test_noise=np.random.randn(test_mean,test_var/2.,size=data.data_array.shape)\
+                  +1j*np.random.randn(test_mean,test_var/2.,size=data.data_array.shape)
+        data.data_array=test_noise#set data equal to test noise.
+        data.flag_array[:]=True#Make sure that all of the flags are set too true for analytic case. 
+        pspec=PSpecData()
+        pspec.add(data)#create pspec data set from data.
+        for j in range(1,len(pspec.dsets[0].Nbls)):
+            for k in range(j):
+                #test equality
+                self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k)),
+                                            np.identity(pspec.dsets[0].Nfreqs)\
+                                            *pspec.dsets[0]**2./(2.*test_var),
+                                            rtol=2./pspec.dsets[0].Ntimes,
+                                            atol=test_var*2./pspec.dsets[0].Ntimes))
+                #test for identity
+                self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k),use_identity=True),
+                                            np.identity(pspec.dsets[0].Nfreqs)\
+                                            *pspec.dsets[0].Nfreqs**2./2.))
+                self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k),use_identity=True),
+                                            np.identity(pspec.dsets[0].Nfreqs)))
+                #test for true fisher
+                self.assertTrue(np.allclose(pspec.get_F((i,j),(i,k),true_fisher=True),
+                                            np.identity(pspec.dsets[0].Nfreqs)\
+                                            *pspec.dsets[0].Nfreqs**2./(2.*test_var),
+                                            rtol=2./pspec.dsets[0].Ntimes,
+                                            atol=test_var*2./pspec.dsets[0].Ntimes))
+                
+                                                
+        #TODO: Need a test case for some kind of taper.
+            
+
+        
 
 if __name__ == "__main__":
     unittest.main()
