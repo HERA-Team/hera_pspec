9eb184fa78f4a1aefc1a6ef935595a56f3a7116e
Nicholas Kern <nkern@berkeley.edu>
2018-03-18 17:13:19 -0700

    modified:   ../pspecdata.py

--- hera_pspec/pspecdata.py
+++ hera_pspec/pspecdata.py
@@ -666,7 +666,7 @@ g
             return delay * 1e9 # convert to ns
     
     
-    def scalar(self, stokes='I', taper='none', little_h=True, num_steps=20000):
+    def scalar(self, stokes='I', taper='none', little_h=True, num_steps=2000):
         """
         Computes the scalar function to convert a power spectrum estimate
         in "telescope units" to cosmological units
@@ -702,7 +702,7 @@ g
                 [\int dnu (\Omega_PP / \Omega_P^2) ( B_PP / B_P^2 ) / (X^2 Y)]^-1
                 in h^-3 Mpc^3 or Mpc^3.
         """
-        scalar = self.primary_beam.compute_pspec_scalar(self.freqs[0], self.freqs[-1], stokes=stokes,
+        scalar = self.primary_beam.compute_pspec_scalar(self.freqs[0], self.freqs[-1], len(self.freqs), stokes=stokes,
                                                         taper=taper, little_h=little_h, num_steps=num_steps)
 
         return scalar
