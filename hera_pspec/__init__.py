201f1427f34c7a25d7fe2822ac06661062ee06eb
Nicholas Kern <nkern@berkeley.edu>
2018-04-01 23:42:56 -0700

    started hera_pspec file format

--- hera_pspec/__init__.py
+++ hera_pspec/__init__.py
@@ -1,11 +1,12 @@
 """
 __init__.py file for hera_pspec
 """
-import version
-import conversions
-import bootstrap
-
-from pspecdata import PSpecData
+import .version
+import .conversions
+import .bootstrap
+from .uvpspec import UVPSpec
+from .pspecdata import PSpecData
+from .parameter import PSpecParam
 
 # XXX: This will eventually be deprecated
 import legacy_pspec as legacy
