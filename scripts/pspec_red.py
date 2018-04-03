d43e8a5f5fe3244888c9c5711b755fecdab24880
Phil Bull <philbull@gmail.com>
2018-04-03 15:58:46 -0700

    Develop run script slightly further

--- scripts/pspec_red.py
+++ scripts/pspec_red.py
@@ -32,13 +32,19 @@ SUBDIRS = [2458098, 2458099, 2458101, 2458102, 2458103, 2458104, 2458105,
            2458113, 2458114, 2458115, 2458116, 2458140]
 NAME_TEMPLATE = "zen.*.yy.HH.uvOCRSD"
 """
+
+# Input data and beams
 DATA_DIR = "../hera_pspec/"
 SUBDIRS = ["data",]
 NAME_TEMPLATE = "zen.*.xx.HH.uvXA"
 BEAM_FILE = "data/NF_HERA_Beams.beamfits"
-PSPEC_OUTPUT = "hera_red.hdf5"
+#FLAG_FILE = "zen.2458098.66239.yy.HH.uv.vis.uvfits.flags.npz"
 
-# zen.2458098.66239.yy.HH.uv.vis.uvfits.flags.npz
+# Power spectrum settings
+PSPEC_OUTPUT = "hera_red.hdf5"
+PSPEC_DATA_WEIGHT = 'iC' # 'identity'
+PSPEC_NORM = 'I'
+PSPEC_TAPER = 'blackman-harris' #'none'
 
 #-------------------------------------------------------------------------------
 # Prepare list of data files
@@ -77,27 +83,42 @@ beam = hp.pspecbeam.PSpecBeamUV(beamfile)
 log("Loaded beam file: %s" % beamfile)
 
 # Load weights
-# FIXME: Need to do this properly
+# FIXME: Need to load weights from a proper source
 wgts = [None for f in files]
 
 #-------------------------------------------------------------------------------
+# Calculate power spectrum and package output into PSpecContainer
+#-------------------------------------------------------------------------------
+
 # Package data files into PSpecData object
-#-------------------------------------------------------------------------------
-hp.PSpecData(dsets=dsets, wgts=wgts, beam=beam)
+ds = hp.PSpecData(dsets=dsets, wgts=wgts, beam=beam)
 
+# Set-up which baselines to cross-correlate
+# TODO: Need to find available redundant baseline sets
+bls = [(37, 39),]
 
-#-------------------------------------------------------------------------------
-# Calculate power spectrum and package into PSpecContainer
-#-------------------------------------------------------------------------------
+# Calculate power spectra for all baseline pairs
+pspecs, blpairs = ds.pspec(bls, 
+                           input_data_weight=PSPEC_DATA_WEIGHT, 
+                           norm=PSPEC_NORM, 
+                           taper=PSPEC_TAPER, 
+                           little_h=True)
 
+# Create new PSpecContainer to store output power spectra
+ps_store = PSpecContainer(PSPEC_OUTPUT, mode='rw')
 
+# FIXME: Placeholder for proper UVPSpec implementation
+ps = UVPSpec(data_dict={'pspec': }, 
+             attr_dict={'bls': })
 
-pspecs = PSpecContainer(PSPEC_OUTPUT, mode='rw')
-
+# Add power spectra to container
+GROUP_NAME = "test"
+ps_store.set_pspec(group=GROUP_NAME, pspec="something", ps)
 
 
 #-------------------------------------------------------------------------------
 # Output empirical covariance matrix diagnostics
 #-------------------------------------------------------------------------------
+#ps.iC(key)
 
 
