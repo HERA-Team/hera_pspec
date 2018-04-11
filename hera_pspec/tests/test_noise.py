6f29e310b30ee0b9ffca0305047b8e41ed33c2c7
Nicholas Kern <nkern@berkeley.edu>
2018-04-11 16:44:24 -0700

    increased test coverage for noise.py, pspecbeam.py and uvpspec.py

---
+++ hera_pspec/tests/test_noise.py
@@ -0,0 +1,65 @@
+import unittest
+import nose.tools as nt
+import numpy as np
+import os
+import sys
+from hera_pspec.data import DATA_PATH
+from hera_pspec import uvpspec, conversions, parameter, pspecbeam, noise
+import copy
+import h5py
+from collections import OrderedDict as odict
+
+class Test_Sense(unittest.TestCase):
+    """ Test noise.Sense object """
+
+    def setUp(self):
+        self.cosmo = conversions.Cosmo_Conversions()
+        self.beam = pspecbeam.PSpecBeamUV(os.path.join(DATA_PATH, 'NF_HERA_Beams.beamfits'))
+        self.sense = noise.Sense(beam=self.beam, cosmo=self.cosmo)
+
+    def tearDown(self):
+        pass
+
+    def runTest(self):
+        pass
+
+    def test_add(self):
+        sense = noise.Sense()
+
+        C = conversions.Cosmo_Conversions()
+        sense.add_cosmology(C)
+        nt.assert_equal(C.get_params(), sense.cosmo.get_params())
+        params = str(C.get_params())
+        sense.add_cosmology(params)
+        nt.assert_equal(C.get_params(), sense.cosmo.get_params())
+
+        sense.add_beam(self.beam)
+        nt.assert_equal(sense.cosmo.get_params(), sense.beam.cosmo.get_params())
+        self.beam.cosmo = C
+        sense.add_beam(self.beam)
+        nt.assert_equal(sense.cosmo.get_params(), sense.beam.cosmo.get_params())
+
+    def test_scalar(self):
+        freqs = np.linspace(150e6, 160e6, 100, endpoint=False)
+        self.sense.calc_scalar(freqs, 'pseudo_I', num_steps=5000, little_h=True)
+        nt.assert_true(np.isclose(freqs, self.sense.subband).all())
+        nt.assert_true(self.sense.stokes, 'I')
+
+    def test_calc_P_N(self):
+        # calculate scalar
+        freqs = np.linspace(150e6, 160e6, 100, endpoint=False)
+        self.sense.calc_scalar(freqs, 'pseudo_I', num_steps=5000, little_h=True)
+        # basic execution 
+        k = np.linspace(0, 3, 10)
+        Tsys = 500.0
+        t_int = 10.7
+        P_N = self.sense.calc_P_N(k, Tsys, t_int, Ncoherent=1, Nincoherent=1, form='Pk')
+        nt.assert_equal(P_N.shape, (10,))
+        nt.assert_true(np.isclose(P_N, 9.07836740e+11).all())
+        # calculate Dsq
+        Dsq = self.sense.calc_P_N(k, Tsys, t_int, Ncoherent=1, Nincoherent=1, form='Dsq')
+        nt.assert_equal(Dsq.shape, (10,))
+        nt.assert_true(Dsq[1] < P_N[1])
+
+
+
--- hera_pspec/tests/test_uvpspec.py
+++ hera_pspec/tests/test_uvpspec.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import sys
 from hera_pspec.data import DATA_PATH
-from hera_pspec import uvpspec, conversions, parameter
+from hera_pspec import uvpspec, conversions, parameter, pspecbeam
 import copy
 import h5py
 from collections import OrderedDict as odict
@@ -39,6 +39,8 @@ class Test_UVPSpec(unittest.TestCase):
         lst_array = np.repeat(np.ones(Ntimes, dtype=np.float), Nblpairs)
         lst_1_array = lst_array
         lst_2_array = lst_array
+        time_avg_array = time_array
+        lst_avg_array = lst_array
         spws = np.arange(Nspws)
         spw_array = np.tile(spws, Ndlys)
         freq_array = np.repeat(np.linspace(100e6, 105e6, Nfreqs, endpoint=False), Nspws)
@@ -52,25 +54,37 @@ class Test_UVPSpec(unittest.TestCase):
         taper = "none"
         norm = "I"
         git_hash = "random"
+        scalar_array = np.ones((Nspws, Npols), np.float)
 
         telescope_location = np.array([5109325.85521063, 2005235.09142983, -3239928.42475397])
 
-        data_array, wgt_array, integration_array = {}, {}, {}
+        cosmo = conversions.Cosmo_Conversions()
+
+        data_array, wgt_array, integration_array, nsample_array = {}, {}, {}, {}
         for s in spws:
             data_array[s] = np.ones((Nblpairts, Ndlys, Npols), dtype=np.complex) * blpair_array[:, None, None] / 1e9
             wgt_array[s] = np.ones((Nblpairts, Ndlys, 2, Npols), dtype=np.float)
             integration_array[s] = np.ones((Nblpairts, Npols), dtype=np.float)
+            nsample_array[s] = np.ones((Nblpairts, Npols), dtype=np.float)
 
         params = ['Ntimes', 'Nfreqs', 'Nspws', 'Nspwdlys', 'Nblpairs', 'Nblpairts', 'Npols', 'Ndlys',
                   'Nbls', 'blpair_array', 'time_1_array', 'time_2_array', 'lst_1_array', 'lst_2_array',
                   'spw_array', 'dly_array', 'freq_array', 'pol_array', 'data_array', 'wgt_array',
                   'integration_array', 'bl_array', 'bl_vecs', 'telescope_location', 'units',
-                  'channel_width', 'weighting', 'history', 'taper', 'norm', 'git_hash']
+                  'channel_width', 'weighting', 'history', 'taper', 'norm', 'git_hash', 'nsample_array',
+                  'time_avg_array', 'lst_avg_array', 'cosmo', 'scalar_array']
 
         for p in params:
             setattr(uvp, p, locals()[p])
 
+        uvp.check()
         self.uvp = uvp
+
+        # test equivalence
+        nt.assert_equal(uvp, uvp)
+
+        self.cosmo = cosmo
+        self.beam = pspecbeam.PSpecBeamUV(os.path.join(DATA_PATH, 'NF_HERA_Beams.beamfits'))
 
     def tearDown(self):
         pass
@@ -101,6 +115,22 @@ class Test_UVPSpec(unittest.TestCase):
         nt.assert_equal(i.shape, (10,))
         nt.assert_true(i.dtype == np.float)
         nt.assert_almost_equal(i[0], 1.0)
+        # get nsample
+        n = self.uvp.get_nsamples((0, ((1, 2), (1, 2)), 'xx'))
+        nt.assert_equal(n.shape, (10,))
+        nt.assert_true(n.dtype == np.float)
+        nt.assert_almost_equal(n[0], 1.0)
+        # get dly
+        d = self.uvp.get_dlys(0)
+        nt.assert_equal(len(d), 50)
+        # get blpair seps
+        blp = self.uvp.get_blpair_seps()
+        nt.assert_equal(len(blp), 30)
+        nt.assert_true(np.isclose(blp, 14.60, rtol=1e-1, atol=1e-1).all())
+        # get kvecs
+        k_perp, k_para = self.uvp.get_kvecs(0)
+        nt.assert_equal(len(k_perp), 30)
+        nt.assert_equal(len(k_para), 50)
 
     def test_convert_deltasq(self):
         uvp = copy.deepcopy(self.uvp)
@@ -109,6 +139,7 @@ class Test_UVPSpec(unittest.TestCase):
         k_perp, k_para = uvp.get_kvecs(0, little_h=True)
         k_mag = np.sqrt(k_perp[:, None, None]**2 + k_para[None, :, None]**2)
         nt.assert_true(np.isclose(uvp.data_array[0][0,:,0], (self.uvp.data_array[0]*k_mag**3/(2*np.pi**2))[0,:,0]).all())
+        nt.assert_equal(uvp.units, 'unknown h^3 k^3 / (2pi^2)')
 
     def test_blpair_conversions(self):
         # test blpair -> antnums
@@ -123,13 +154,9 @@ class Test_UVPSpec(unittest.TestCase):
         # test antnums -> bl
         bp = self.uvp.antnums_to_bl((1, 2))
         nt.assert_equal(bp, 1002)
-        # test blpair to indices
-        inds = self.uvp.blpair_to_indices(1002001002)
-        nt.assert_true(np.isclose(inds, np.array([0,3,6,9,12,15,18,21,24,27])).min())
-        inds = self.uvp.blpair_to_indices(((1,2),(1,2)))
-        nt.assert_true(np.isclose(inds, np.array([0,3,6,9,12,15,18,21,24,27])).min())
 
-    def test_key_to_indices(self):
+    def get_indices_funcs(self):
+        # key to indices
         spw, blpairts, pol = self.uvp.key_to_indices( (0, ((1,2),(1,2)), -5) )
         nt.assert_equal(spw, 0)
         nt.assert_equal(pol, 0)
@@ -139,11 +166,11 @@ class Test_UVPSpec(unittest.TestCase):
         nt.assert_equal(pol, 0)
         nt.assert_true(np.isclose(blpairts, np.array([0,3,6,9,12,15,18,21,24,27])).min())
 
-    def test_spw_to_indices(self):
+        # spw to indices
         spw = self.uvp.spw_to_indices(0)
         nt.assert_equal(len(spw), self.uvp.Ndlys)
 
-    def test_pol_to_indices(self):
+        # pol to indices
         pol = self.uvp.pol_to_indices('xx')
         nt.assert_equal(len(pol), 1)
         pol = self.uvp.pol_to_indices(-5)
@@ -151,18 +178,36 @@ class Test_UVPSpec(unittest.TestCase):
         pol = self.uvp.pol_to_indices(['xx', 'xx'])
         nt.assert_equal(len(pol), 1)
 
+        # test blpair to indices
+        inds = self.uvp.blpair_to_indices(1002001002)
+        nt.assert_true(np.isclose(inds, np.array([0,3,6,9,12,15,18,21,24,27])).min())
+        inds = self.uvp.blpair_to_indices(((1,2),(1,2)))
+        nt.assert_true(np.isclose(inds, np.array([0,3,6,9,12,15,18,21,24,27])).min())
+
     def test_select(self):
+        # bl group select
         uvp = copy.deepcopy(self.uvp)
         uvp.select(bls=[(1, 2)], inplace=True)
         nt.assert_equal(uvp.Nblpairs, 1)
         nt.assert_equal(uvp.data_array[0].shape, (10, 50, 1))
         nt.assert_almost_equal(uvp.data_array[0][0,0,0], (1.0020010020000001+0j))
+        # inplace vs not inplace, spw selection
         uvp = copy.deepcopy(self.uvp)
         uvp2 = uvp.select(spws=0, inplace=False)
         uvp2 = uvp.select(bls=[(1, 2)], inplace=False)
         nt.assert_equal(uvp2.Nblpairs, 1)
         nt.assert_equal(uvp2.data_array[0].shape, (10, 50, 1))
         nt.assert_almost_equal(uvp2.data_array[0][0,0,0], (1.0020010020000001+0j))
+        # blpair select
+        uvp = copy.deepcopy(self.uvp)
+        uvp2 = uvp.select(blpairs=[1002001002, 2003002003], inplace=False)
+        nt.assert_equal(uvp2.Nblpairs, 2)
+        # pol select
+        uvp2 = uvp.select(pols=[-5], inplace=False)
+        nt.assert_equal(uvp2.pol_array[0], -5)
+        # time select
+        uvp2 = uvp.select(times=np.unique(uvp.time_avg_array)[:1], inplace=False)
+        nt.assert_equal(uvp2.Ntimes, 1)
 
     def test_get_ENU_bl_vecs(self):
         bl_vecs = self.uvp.get_ENU_bl_vecs()
@@ -211,6 +256,50 @@ class Test_UVPSpec(unittest.TestCase):
         nt.assert_equal(uvp.Nblpairs, 3)
         nt.assert_false(hasattr(uvp, 'data_array'))
         if os.path.exists('./ex.hdf5'): os.remove('./ex.hdf5')
+
+    def test_sense(self):
+        uvp = copy.deepcopy(self.uvp)
+
+        # test exception
+        nt.assert_raises(AssertionError, uvp.generate_noise_spectra, 0, 0, 0)
+
+        # test generate_sense
+        uvp.generate_sense(self.beam)
+        nt.assert_true(hasattr(uvp, 'sense'))
+        nt.assert_true(hasattr(uvp.sense, 'beam'))
+        nt.assert_true(hasattr(uvp.sense, 'cosmo'))
+
+        # test generate noise spectra
+        P_N = uvp.generate_noise_spectra(0, -5, 500, form='Pk', real=True)
+        nt.assert_equal(P_N.shape, (30, 50))
+
+        # test smaller system temp
+        P_N2 = uvp.generate_noise_spectra(0, -5, 400, form='Pk', real=True)
+        nt.assert_true((P_N > P_N2).all())
+
+        # test complex
+        P_N2 = uvp.generate_noise_spectra(0, -5, 500, form='Pk', real=False)
+        nt.assert_true((P_N < P_N2).all())
+
+        # test Dsq
+        Dsq = uvp.generate_noise_spectra(0, -5, 500, form='Dsq', real=True)
+        nt.assert_equal(Dsq.shape, (30, 50))
+        nt.assert_true(Dsq[0, 1] < P_N[0, 1])
+
+    def test_average_spectra(self):
+        uvp = copy.deepcopy(self.uvp)
+        # test blpair averaging
+        blpairs = uvp.get_blpair_groups_from_bl_groups([[1002, 2003, 1003]])
+        uvp2 = uvp.average_spectra(blpair_groups=blpairs, time_avg=False, inplace=False)
+        nt.assert_equal(uvp2.Nblpairs, 1)
+        nt.assert_true(np.isclose(uvp2.get_nsamples(0, 1002001002, 'xx'), 3.0).all())
+        nt.assert_equal(uvp2.get_data(0, 1002001002, 'xx').shape, (10, 50))
+        # test time averaging
+        uvp2 = uvp.average_spectra(time_avg=True, inplace=False)
+        nt.assert_true(uvp2.Ntimes, 1)
+        nt.assert_true(np.isclose(uvp2.get_nsamples(0, 1002001002, 'xx'), 10.0).all())
+        nt.assert_true(uvp2.get_data(0, 1002001002, 'xx').shape, (1, 50))
+
 
 def test_conj_blpair_int():
     conj_blpair = uvpspec._conj_blpair_int(1002003004)
