92d2a88eda967b7d9b67ff2eef9803d22017d0ff
Phil Bull <philbull@gmail.com>
2018-05-08 23:33:06 -0700

    Tidy-up __init__

--- hera_pspec/__init__.py
+++ hera_pspec/__init__.py
@@ -2,13 +2,12 @@
 __init__.py file for hera_pspec
 """
 from hera_pspec import version, conversions, grouping, pspecbeam
+from hera_pspec import uvpspec_utils as uvputils
 
 from hera_pspec.uvpspec import UVPSpec
 from hera_pspec.pspecdata import PSpecData
 from hera_pspec.container import PSpecContainer
 from hera_pspec.parameter import PSpecParam
-#from hera_pspec import pspecbeam as beam
-from hera_pspec import uvpspec_utils as uvputils
 from hera_pspec.pspecbeam import PSpecBeamUV, PSpecBeamGauss, PSpecBeamFromArray
 
 # XXX: This will eventually be deprecated
