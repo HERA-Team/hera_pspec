67fecdd75c1259c93ac642b096af5ee4cdc2ae30
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-06-09 12:39:29 -0700

    fixed minor bug trying to extend Nonetype.

--- hera_pspec/pspecdata.py
+++ hera_pspec/pspecdata.py
@@ -1738,7 +1738,6 @@ class PSpecData(object):
                         pol_cov.extend(cov_pv)
                     else:
                         store_cov=False #don't store incomplete covariance.
-                        pol_cov.extend(None)
 
 
                     # Get baseline keys
