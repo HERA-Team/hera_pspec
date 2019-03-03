ac700912802cab8f14350eb31627b6dba702f175
Phil Bull <philbull@gmail.com>
2019-03-03 17:13:17 +0000

    Upgrade version.py to hera_cal version

--- hera_pspec/version.py
+++ hera_pspec/version.py
@@ -1,51 +1,118 @@
-#from __future__ import print_function
+# -*- coding: utf-8 -*-
+# Copyright 2019 the HERA Project
+# Licensed under the MIT License
+
+from __future__ import print_function, division, absolute_import
+
 import os
+import six
 import subprocess
 import json
+import inspect
 
 hera_pspec_dir = os.path.dirname(os.path.realpath(__file__))
-version_file = os.path.join(hera_pspec_dir, 'VERSION')
-version = open(version_file).read().strip()
+
+
+def _get_git_output(args, capture_stderr=False):
+    """
+    Get output from git, ensuring that it is of the ``str`` type, not bytes.
+    """
+
+    argv = ['git', '-C', hera_pspec_dir] + args
+
+    if capture_stderr:
+        data = subprocess.check_output(argv, stderr=subprocess.STDOUT)
+    else:
+        data = subprocess.check_output(argv)
+
+    data = data.strip()
+
+    if six.PY2:
+        return data
+    return data.decode('utf8')
+
+
+def _get_gitinfo_file(git_file=None):
+    """
+    Get saved info from GIT_INFO file that was created when installing package
+    """
+    if git_file is None:
+        git_file = os.path.join(hera_pspec_dir, 'GIT_INFO')
+
+    with open(git_file) as data_file:
+        data = [_unicode_to_str(x) for x in json.loads(data_file.read().strip())]
+        git_origin = data[0]
+        git_hash = data[1]
+        git_description = data[2]
+        git_branch = data[3]
+
+    return {'git_origin': git_origin, 'git_hash': git_hash,
+            'git_description': git_description, 'git_branch': git_branch}
+
+
+def _unicode_to_str(u):
+    if six.PY2:
+        return u.encode('utf8')
+    return u
 
 
 def construct_version_info():
-    hera_pspec_dir = os.path.dirname(os.path.realpath(__file__))
     version_file = os.path.join(hera_pspec_dir, 'VERSION')
-    version = open(version_file).read().strip()
+    with open(version_file) as f:
+        version = f.read().strip()
+
+    git_origin = ''
+    git_hash = ''
+    git_description = ''
+    git_branch = ''
+
+    version_info = {'version': version, 'git_origin': '', 'git_hash': '',
+                    'git_description': '', 'git_branch': ''}
 
     try:
-        git_origin = subprocess.check_output(['git', '-C', hera_pspec_dir, 'config',
-                                              '--get', 'remote.origin.url'],
-                                             stderr=subprocess.STDOUT).strip()
-        git_hash = subprocess.check_output(['git', '-C', hera_pspec_dir, 'rev-parse', 'HEAD'],
-                                           stderr=subprocess.STDOUT).strip()
-        git_description = subprocess.check_output(['git', '-C', hera_pspec_dir,
-                                                   'describe', '--dirty', '--tag', '--always']).strip()
-        git_branch = subprocess.check_output(['git', '-C', hera_pspec_dir, 'rev-parse',
-                                              '--abbrev-ref', 'HEAD'],
-                                             stderr=subprocess.STDOUT).strip()
-        git_version = subprocess.check_output(['git', '-C', hera_pspec_dir, 'describe', '--always',
-                                               '--tags', '--abbrev=0']).strip()
-    except:  # pragma: no cover  - can't figure out how to test exception.
+        version_info['git_origin'] = _get_git_output(
+                                      ['config', '--get', 'remote.origin.url'], 
+                                      capture_stderr=True)
+        version_info['git_hash'] = _get_git_output(
+                                      ['rev-parse', 'HEAD'], 
+                                      capture_stderr=True)
+        version_info['git_description'] = _get_git_output(
+                                   ['describe', '--dirty', '--tag', '--always'])
+        version_info['git_branch'] = _get_git_output(
+                                      ['rev-parse', '--abbrev-ref', 'HEAD'], 
+                                      capture_stderr=True)
+    except subprocess.CalledProcessError:  # pragma: no cover
         try:
             # Check if a GIT_INFO file was created when installing package
-            git_file = os.path.join(hera_pspec_dir, 'GIT_INFO')
-            with open(git_file) as data_file:
-                data = [x.encode('UTF8') for x in json.loads(data_file.read().strip())]
-                git_origin = data[0]
-                git_hash = data[1]
-                git_description = data[2]
-                git_branch = data[3]
-        except:
-            git_origin = ''
-            git_hash = ''
-            git_description = ''
-            git_branch = ''
+            version_info.update(_get_gitinfo_file())
+        except (IOError, OSError):
+            pass
 
-    version_info = {'version': version, 'git_origin': git_origin,
-                    'git_hash': git_hash, 'git_description': git_description,
-                    'git_branch': git_branch}
     return version_info
+
+
+def history_string(notes=''):
+    """
+    Creates a standardized history string that all functions that write to 
+    disk can use. Optionally add notes.
+    """
+    history = '\n------------\nThis file was produced by the function ' \
+            + str(inspect.stack()[1][3]) + '()'
+    # inspect.stack()[1][3] is the name of the function that called this fn
+    
+    history += ' in ' + os.path.basename(inspect.stack()[1][1]) + ' using: '
+    # inspect.stack()[1][1] is path to the file that contains the function 
+    # that called this function
+    version_info = construct_version_info()
+    
+    for v in sorted(version_info.keys()):
+        history += '\n    ' + v + ': ' + version_info[v]
+    
+    if (notes is not None) and (notes != ''):
+        history += '\n\nNotes:\n'
+        history += notes
+    return history + '\n------------\n'
+
 
 version_info = construct_version_info()
 version = version_info['version']
@@ -54,23 +121,13 @@ git_hash = version_info['git_hash']
 git_description = version_info['git_description']
 git_branch = version_info['git_branch']
 
-# String to add to history of any files written with this version of pyuvdata
-hera_pspec_version_str = ('hera_pspec version: ' + version + '.')
-if git_hash is not '':
-    hera_pspec_version_str += ('  Git origin: ' + str(git_origin) +
-                            '.  Git hash: ' + str(git_hash) +
-                            '.  Git branch: ' + str(git_branch) +
-                            '.  Git description: ' + str(git_description) + '.')
-
 
 def main():
-    try:
-        print("Version = {0}".format(version))
-        print("git origin = {0}".format(git_origin.decode('utf-8')))
-        print("git branch = {0}".format(git_branch.decode('utf-8')))
-        print("git description = {0}".format(git_description.decode('utf-8')))
-    except TypeError:
-        print(type(version), type(git_origin))
-        raise
+    print('Version = {0}'.format(version))
+    print('git origin = {0}'.format(git_origin))
+    print('git branch = {0}'.format(git_branch))
+    print('git description = {0}'.format(git_description))
+
+
 if __name__ == '__main__':
     main()
