bdb51738859704761cf9024eae0c23976e653a86
Phil Bull <philbull@gmail.com>
2019-04-10 12:03:31 +0100

    Try another fix for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ MOCK_MODULES = ['numpy', 'scipy', 'scipy.interpolate', 'scipy.integrate',
                 'pyuvdata', 'h5py', 'aipy', 'omnical', 'linsolve', 'hera_qm', 
                 'uvtools', 'hera_cal', 'hera_cal.utils', 'healpy', 
                 'scikit-learn', 'astropy', 'astropy.cosmology', 'matplotlib', 
-                'matplotlib.pyplot', 'pylab', 'yaml', 'pyuvdata.utils']
+                'matplotlib.pyplot', 'pylab', 'yaml', 'pyuvdata.utils',
+                'hera_pspec.conversions', 'hera_pspec.uvpspec_utils']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
