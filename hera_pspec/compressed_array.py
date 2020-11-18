45bce892914ceaa99a70cf75d454ad87172217fd
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-11-17 18:59:08 -0800

    fleshing out setitem.

--- hera_pspec/compressed_array.py
+++ hera_pspec/compressed_array.py
@@ -10,6 +10,9 @@ class CompressedArray():
             list of integers representing dimensions to be treated as "psuedo"
             in that they are actually dictionaries with redundant pointers to
             slices.
+
+        We can only set values on the slice level. No setting of values of slices
+        is allowed!
         """
         # shape is a tuple
         if not isinstance(shape, tuple):
@@ -19,6 +22,7 @@ class CompressedArray():
         self.shape = shape
         self.fill_value = fill_value
         self.ndims = len(self.shape)
+        self.array = {}
 
     def _parse_key(key):
         """
@@ -43,7 +47,29 @@ class CompressedArray():
 
         """
         pkey, akey = parse_key(key)
-        # if psuedo dims
+        if not len(akey) > 0:
+            for a in akey:
+                if not (isinstance(a, slice) and a[0] is None and a[1] is None and a[2] is None):
+                    raise ValueError("CompressedArray does not support setting values within slices!")
+        if key in self.array:
+            if not np.all(np.isclose(value, self.array(key))):
+                for k in self.array.keys():
+                    if np.all(np.isclose(value, self.array(key))):
+                        
+                        break
+
+        # akey must be empty!
+
+        # if all array dims are unspecified or filled
+        # then iterate through keys until we get an equal array.
+        # if no equal/close array is found, initialize a new array.
+
+        # if only some array dims are unspecified, find appropriate
+        # slice, if it exists and fill these dims
+        # if this slice does not exist, intialize a slice with fill_value.
+        # and set values.
+
+        # if psuedo dims are
 
 
 
