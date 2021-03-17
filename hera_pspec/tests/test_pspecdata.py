Fix malformed tests, replace silent failure in trim_dset_lsts with error

hera_pspec/tests/test_pspecdata.py
@@ -1009,12 +1009,10 @@ class Test_PSpecData(unittest.TestCase):
                                              matrix_scale * multiplicative_tolerance)
 
 
-    '''
+    """
     Under Construction
     def test_parseval(self):
-        """
-        Test that output power spectrum respects Parseval's theorem.
-        """
+        # Test that output power spectrum respects Parseval's theorem.
         np.random.seed(10)
         variance_in = 1.
         Nfreq = self.d[0].Nfreqs
@@ -1078,7 +1076,7 @@ class Test_PSpecData(unittest.TestCase):
         # Perform approx. equality test (this is a stochastic quantity, so we
         # only expect equality to ~10^-2 to 10^-3
         np.testing.assert_allclose(parseval_phat, parseval_real, rtol=1e-3)
-    '''
+    """
 
     def test_scalar_delay_adjustment(self):
         self.ds = pspecdata.PSpecData(dsets=self.d, wgts=self.w, beam=self.bm)

hera_pspec/tests/test_uvpspec.py
@@ -451,12 +451,12 @@ class Test_UVPSpec(unittest.TestCase):
         uvp = copy.deepcopy(self.uvp)
         if os.path.exists('./ex.hdf5'): os.remove('./ex.hdf5')
         uvp.write_hdf5('./ex.hdf5', overwrite=True)
-        assert(os.path.exists('./ex.hdf5'))
+        assert os.path.exists('./ex.hdf5')
         
         # test basic read
         uvp2 = uvpspec.UVPSpec()
         uvp2.read_hdf5('./ex.hdf5')
-        assert(uvp, uvp2)
+        assert uvp == uvp2
         
         # test just meta
         uvp2 = uvpspec.UVPSpec()
@@ -540,11 +540,10 @@ class Test_UVPSpec(unittest.TestCase):
 
         # test time averaging
         uvp2 = uvp.average_spectra(time_avg=True, inplace=False)
-        assert(uvp2.Ntimes, 1)
+        assert uvp2.Ntimes == 1
         assert(np.isclose(
                 uvp2.get_nsamples((0, 101102101102, ('xx','xx'))), 10.0).all())
-        assert(uvp2.get_data((0, 101102101102, ('xx','xx'))).shape,
-                       (1, 30))
+        assert uvp2.get_data((0, 101102101102, ('xx','xx'))).shape == (1, 30)
         # ensure averaging works when multiple repeated baselines are present, but only
         # if time_avg = True
         uvp.blpair_array[uvp.blpair_to_indices(102103102103)] = 101102101102
@@ -701,7 +700,7 @@ class Test_UVPSpec(unittest.TestCase):
         uvp1.write_hdf5('uvp1.hdf5', overwrite=True)
         uvp2.write_hdf5('uvp2.hdf5', overwrite=True)
         out = uvpspec.combine_uvpspec(['uvp1.hdf5', 'uvp2.hdf5'], verbose=False)
-        assert(out.Npols, 2)
+        assert out.Npols == 2
         for ff in ['uvp1.hdf5', 'uvp2.hdf5']:
             if os.path.exists(ff):
                 os.remove(ff)
@@ -912,7 +911,7 @@ class Test_UVPSpec(unittest.TestCase):
         uvp1.write_hdf5('uvp1.hdf5', overwrite=True)
         uvp2.write_hdf5('uvp2.hdf5', overwrite=True)
         out = uvpspec.combine_uvpspec(['uvp1.hdf5', 'uvp2.hdf5'], verbose=False)
-        assert(out.Npols, 2)
+        assert out.Npols == 2
         for ff in ['uvp1.hdf5', 'uvp2.hdf5']:
             if os.path.exists(ff): os.remove(ff)
 