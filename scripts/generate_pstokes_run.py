43eda07128a3a1787638d3e3858c0a7b30f0ea88
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-03-22 22:46:22 -0700

    fix pstokes bug.

--- scripts/generate_pstokes_run.py
+++ scripts/generate_pstokes_run.py
@@ -3,16 +3,19 @@
 Pipeline script for generating pstokes visibility.
 """
 
-from . import pstokes
+from hera_pspec import pstokes
 from pyuvdata import UVData
+import pyuvdata
 
-ap = pstokes.gemerate_pstokes_argparser()
+ap = pstokes.generate_pstokes_argparser()
 args = ap.parse_args()
 uvd = UVData()
 uvd.read(args.inputdata)
 if args.outputdata is None:
     args.outputdata = args.inputdata
 # generate pstokes and append to file.
-uvd += pstokes.construct_pstokes(uvd, uvd, pstokes=args.pstokes)
+for p in args.pstokes:
+    if pyuvdata.utils.polstr2num(p) not in uvd.polarization_array:
+        uvd += pstokes.construct_pstokes(uvd, uvd, pstokes=p)
 # overwrite file.
 uvd.write_uvh5(args.outputdata, clobber=args.clobber)
