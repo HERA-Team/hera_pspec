7c7bcc110bd90d81282f90b221fa575281993120
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-03-22 21:40:56 -0700

    add python hash bangs.

--- scripts/auto_noise_run.py
+++ scripts/auto_noise_run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Pipeline script to obtain error bars from autocorrelations.
 """
--- scripts/generate_pstokes_run.py
+++ scripts/generate_pstokes_run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Pipeline script for generating pstokes visibility.
 """
