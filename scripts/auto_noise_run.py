530370d5c7bf45dd3a7e6a7cbd4054762d06d84d
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-03-27 15:45:38 -0700

    handle existing errors.

--- scripts/auto_noise_run.py
+++ scripts/auto_noise_run.py
@@ -8,20 +8,16 @@ from hera_pspec.container import PSpecContainer
 from pyuvdata import UVData
 
 parser = utils.uvp_noise_error_parser()
-parser.parse_args()
+args = parser.parse_args()
 
 # compute Tsys from autocorr uvd
 uvd = UVData()
 uvd.read(args.auto_file)
 auto_Tsys = utils.uvd_to_Tsys(uvd, beam=args.beam)
 # load in pspec container.
-psc = PSpecContainer(args.pspec_container, keep_open=False)
+psc = PSpecContainer(args.pspec_container, keep_open=False, mode='rw', swmr=False)
 
-# get spectra and groups automatically if not provided.
-if args.spectra is not None:
-    spectra = args.spectra
-else:
-    spectra = psc.spectra()
+# get groups automatically if not provided.
 if args.groups is not None:
     groups = args.groups
 else:
@@ -30,8 +26,11 @@ else:
 # iterate through spectra and groups,
 # compute noise, and update container.
 for group in groups:
+    if args.spectra is None:
+        spectra = psc.spectra(group)
     for spec in spectra:
         uvp = psc.get_pspec(group, spec)
         utils.uvp_noise_error(uvp, auto_Tsys,
                               err_type=args.err_type)
-        psc.set_pspec(group, spec, overwrite=True)
+        psc.set_pspec(group, spec, uvp, overwrite=True)
+psc.save()
