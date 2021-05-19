abc076c9d8cace6888ab52f6ba8534f2e4dc63c6
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-05-19 14:40:41 -0700

    inplace -> keep_vispols

--- scripts/generate_pstokes_run.py
+++ scripts/generate_pstokes_run.py
@@ -13,7 +13,7 @@ uvd = UVData()
 uvd.read(args.inputdata)
 if args.outputdata is None:
     args.outputdata = args.inputdata
-if args.inplace:
+if args.keep_vispols:
     # if inplace, append new pstokes onto existing file.
     uvd_output = copy.deepcopy(uvd)
 else:
