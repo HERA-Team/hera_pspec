82c65086095d7895a3bd20c90654630155f9f0a9
Adélie Gorce <adelie.gorce@gmail.com>
2021-07-21 11:13:28 -0400

    Corrections to notebook and fix to pyuvdata version conflict

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup_args = {
         'h5py',
         'uvtools @ git+git://github.com/HERA-Team/uvtools',
         'hera_cal @ git+git://github.com/HERA-Team/hera_cal',
-        'hera_sim @ git+git://github.com/HERA-Team/hera_sim'
+        'hera_sim @ git+git://github.com/HERA-Team/hera_sim' 
     ],
     'include_package_data': True,
     'scripts': ['scripts/pspec_run.py', 'scripts/pspec_red.py',
