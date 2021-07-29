262658bdb7da6d8f5810cb1d3641050473caa70d
Josh Dillon <jsdillon@berkeley.edu>
2021-07-29 11:42:07 -0700

    Allow P_N calculation to LSTs that wrap through 0 radians

--- hera_pspec/utils.py
+++ hera_pspec/utils.py
@@ -1352,7 +1352,8 @@ def uvp_noise_error(uvp, auto_Tsys=None, err_type='P_N', precomp_P_N=None, P_SN_
 
     # get metadata if needed
     if precomp_P_N is None:
-        lsts = np.unique(auto_Tsys.lst_array)
+        lst_indices = np.unique(auto_Tsys.lst_array, return_index=True)[1]
+        lsts = auto_Tsys.lst_array[sorted(lst_indices)]
         freqs = auto_Tsys.freq_array[0]
     # calculate scalars for spws and polpairs.
     scalar = {}
