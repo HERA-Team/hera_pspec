b06f246b59ed35533c1d8543886ba0369330c69d
Steven Murray <steven.g.murray@asu.edu>
2025-03-20 23:17:25 +0100

    fix: correct fname merging

--- hera_pspec/cli.py
+++ hera_pspec/cli.py
@@ -30,7 +30,7 @@ def fast_merge_baselines(
     if extras is None:
         extras = []
     extra_attrs = {extra: {} for extra in extras}
-    
+
     if progress:
         try:
             from tqdm import tqdm
@@ -39,14 +39,14 @@ def fast_merge_baselines(
             warnings.warn("tqdm not found, progress bar will not be shown.")
     else:
         tqdm = lambda x: x
-        
+
     files = sorted(glob.glob(pattern))
     cns.print(f"Found {len(files)} files matching pattern.")
-    
+
     for df in tqdm(files, desc="Loading files", unit="file"):
         # load power spectra
         psc = container.PSpecContainer(df, mode='r', keep_open=False)
-        
+
         # Load both the time-averaged and not-time-averaged power spectra.
         # The time-averaging done in the single-baseline notebook has more
         # accurate noise calculations that can only be done when the interleaves
@@ -58,23 +58,23 @@ def fast_merge_baselines(
             if name not in uvps:
                 uvps[name] = []
             uvps[name].append(uvp)
-        
+
         if extras:
             # load additional metadata stored in header
             with h5py.File(df, 'r') as f:
                 for extra in extras:
                     extra_attrs[extra][blp] = f['header'].attrs[extra]
 
-    cns.print("Merging power spectra")        
-    outspec = outpath.parent / outpath.name + ".pspec.h5"
+    cns.print("Merging power spectra")
+    outspec = outpath.parent / f"{outpath.name}.pspec.h5"
     psc = container.PSpecContainer(outspec, mode='rw', keep_open=False)
     for name, uvplist in uvps.items():
         uvp = recursive_combine_uvpspec(uvplist)
         psc.set_pspec(group, name, uvp, overwrite=True)
-    
+
     cns.print(f"Wrote pspecs to file: {outspec}")
     for name, extra in extra_attrs.items():
-        fname = outpath.parent / outpath.name + f".{name}.pkl"
+        fname = outpath.parent / f"{outpath.name}.{name}.pkl"
         with open(fname, 'wb') as f:
             pickle.dump(extra, f)
         cns.print(f"Wrote {fname}")
